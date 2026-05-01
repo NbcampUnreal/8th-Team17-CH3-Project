5b4dee0020674cbfe0502aa0ba4175c177f072fa
DESKTOP-P5R9FA6\user <ljh347755@gmail.com>
2026-05-01 14:54:42 +0900

    필수 기능 C++ 클래스 폴더별로 임의로 설정

--- Source/TemaProject03/TemaProject03.Build.cs
+++ Source/TemaProject03/TemaProject03.Build.cs
@@ -1,4 +1,4 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
+﻿// Copyright Epic Games, Inc. All Rights Reserved.
 
 using UnrealBuildTool;
 
@@ -10,7 +10,7 @@ public class TemaProject03 : ModuleRules
 	
 		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
 
-		PrivateDependencyModuleNames.AddRange(new string[] {  });
+		PrivateDependencyModuleNames.AddRange(new string[] { "TemaProject03" });
 
 		// Uncomment if you are using Slate UI
 		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
---
+++ Source/TemaProject03/UI/FeedbackWidget.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "FeedbackWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TEMAPROJECT03_API UFeedbackWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
