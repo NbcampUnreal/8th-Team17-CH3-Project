66dba15d683674133b3e846ac930f4a070390b20
DESKTOP-P5R9FA6\user <ljh347755@gmail.com>
2026-05-01 17:01:27 +0900

    빌드 - Widget 사용 링크 설정

--- Source/TemaProject03/TemaProject03.Build.cs
+++ Source/TemaProject03/TemaProject03.Build.cs
@@ -8,7 +8,7 @@ public class TemaProject03 : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "UMG" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] { "TemaProject03" });
 
