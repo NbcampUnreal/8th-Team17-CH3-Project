9ba234388ef31cad7d614b08a4364d53f7c5e2e8
DESKTOP-728TD0G\john9 <mayjohn66@gmail.com>
2026-05-07 20:41:46 +0900

    로비 위젯 흐름 로직 추가

---
+++ Source/TemaProject03/LobbyWidget.cpp
@@ -0,0 +1,93 @@
+﻿#include "LobbyWidget.h"
+
+#include "Kismet/GameplayStatics.h"
+#include "Kismet/KismetSystemLibrary.h"
+
+void ULobbyWidget::NativeConstruct()
+{
+    Super::NativeConstruct();
+
+    CurrentMenuState = ELobbyMenuState::MainLobby;
+    SelectedCharacterIndex = INDEX_NONE;
+    SelectedTraitIndex = INDEX_NONE;
+
+    ShowMainLobby();
+    SetCharacterNextButtonEnabled(false);
+    SetFinalStartButtonEnabled(false);
+}
+
+void ULobbyWidget::OnClickGameStart()
+{
+    CurrentMenuState = ELobbyMenuState::CharacterSelect;
+    ShowCharacterSelect();
+}
+
+void ULobbyWidget::OnClickGameExit()
+{
+    UKismetSystemLibrary::QuitGame(
+        this,
+        GetOwningPlayer(),
+        EQuitPreference::Quit,
+        false
+    );
+}
+
+void ULobbyWidget::OnSelectCharacter(int32 SlotIndex)
+{
+    if (SlotIndex < 0 || SlotIndex >= CharacterSlotCount)
+    {
+        return;
+    }
+
+    SelectedCharacterIndex = SlotIndex;
+    SetCharacterNextButtonEnabled(true);
+}
+
+void ULobbyWidget::OnClickCharacterNext()
+{
+    if (SelectedCharacterIndex == INDEX_NONE)
+    {
+        return;
+    }
+
+    CurrentMenuState = ELobbyMenuState::TraitSelect;
+    ShowTraitSelect();
+    SetFinalStartButtonEnabled(SelectedTraitIndex != INDEX_NONE);
+}
+
+void ULobbyWidget::OnSelectTrait(int32 SlotIndex)
+{
+    if (SlotIndex < 0 || SlotIndex >= TraitSlotCount)
+    {
+        return;
+    }
+
+    SelectedTraitIndex = SlotIndex;
+    SetFinalStartButtonEnabled(true);
+}
+
+void ULobbyWidget::OnClickFinalGameStart()
+{
+    if (SelectedCharacterIndex == INDEX_NONE || SelectedTraitIndex == INDEX_NONE)
+    {
+        return;
+    }
+
+    UGameplayStatics::OpenLevel(this, GameLevelName);
+}
+
+void ULobbyWidget::OnClickBack()
+{
+    if (CurrentMenuState == ELobbyMenuState::TraitSelect)
+    {
+        CurrentMenuState = ELobbyMenuState::CharacterSelect;
+        ShowCharacterSelect();
+        return;
+    }
+
+    if (CurrentMenuState == ELobbyMenuState::CharacterSelect)
+    {
+        CurrentMenuState = ELobbyMenuState::MainLobby;
+        ShowMainLobby();
+    }
+}
---
+++ Source/TemaProject03/LobbyWidget.h
@@ -0,0 +1,77 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "LobbyWidget.generated.h"
+
+UENUM(BlueprintType)
+enum class ELobbyMenuState : uint8
+{
+    MainLobby,
+    CharacterSelect,
+    TraitSelect
+};
+
+UCLASS()
+class MYPROJECT_API ULobbyWidget : public UUserWidget
+{
+    GENERATED_BODY()
+
+public:
+    virtual void NativeConstruct() override;
+
+    UFUNCTION(BlueprintCallable)
+    void OnClickGameStart();
+
+    UFUNCTION(BlueprintCallable)
+    void OnClickGameExit();
+
+    UFUNCTION(BlueprintCallable)
+    void OnSelectCharacter(int32 SlotIndex);
+
+    UFUNCTION(BlueprintCallable)
+    void OnClickCharacterNext();
+
+    UFUNCTION(BlueprintCallable)
+    void OnSelectTrait(int32 SlotIndex);
+
+    UFUNCTION(BlueprintCallable)
+    void OnClickFinalGameStart();
+
+    UFUNCTION(BlueprintCallable)
+    void OnClickBack();
+
+protected:
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Lobby")
+    int32 CharacterSlotCount = 5;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Lobby")
+    int32 TraitSlotCount = 5;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Lobby")
+    FName GameLevelName = "GameLevel";
+
+    UPROPERTY(BlueprintReadOnly, Category = "Lobby")
+    int32 SelectedCharacterIndex = INDEX_NONE;
+
+    UPROPERTY(BlueprintReadOnly, Category = "Lobby")
+    int32 SelectedTraitIndex = INDEX_NONE;
+
+    UPROPERTY(BlueprintReadOnly, Category = "Lobby")
+    ELobbyMenuState CurrentMenuState = ELobbyMenuState::MainLobby;
+
+    UFUNCTION(BlueprintImplementableEvent)
+    void ShowMainLobby();
+
+    UFUNCTION(BlueprintImplementableEvent)
+    void ShowCharacterSelect();
+
+    UFUNCTION(BlueprintImplementableEvent)
+    void ShowTraitSelect();
+
+    UFUNCTION(BlueprintImplementableEvent)
+    void SetCharacterNextButtonEnabled(bool bEnabled);
+
+    UFUNCTION(BlueprintImplementableEvent)
+    void SetFinalStartButtonEnabled(bool bEnabled);
+};
