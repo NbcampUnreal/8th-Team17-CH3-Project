5137411a09ee93d4a7b06801b6a47badb1e8835d
DESKTOP-728TD0G\john9 <mayjohn66@gmail.com>
2026-05-13 17:31:18 +0900

    아이템

---
+++ Source/TemaProject03/Item/Item.cpp
@@ -0,0 +1,79 @@
+﻿#include "Item/Item.h"
+
+#include "Components/SphereComponent.h"
+#include "Components/StaticMeshComponent.h"
+#include "TemaProject03/Player/PlayerCharacter.h"
+#include "TemaProject03/BattelSystem/WeaponBase.h"
+
+AItem::AItem()
+{
+    PrimaryActorTick.bCanEverTick = false;
+
+    CollisionSphere = CreateDefaultSubobject<USphereComponent>(TEXT("CollisionSphere"));
+    RootComponent = CollisionSphere;
+
+    CollisionSphere->SetSphereRadius(80.0f);
+    CollisionSphere->SetCollisionEnabled(ECollisionEnabled::QueryOnly);
+    CollisionSphere->SetCollisionResponseToAllChannels(ECR_Ignore);
+    CollisionSphere->SetCollisionResponseToChannel(ECC_Pawn, ECR_Overlap);
+    CollisionSphere->SetGenerateOverlapEvents(true);
+
+    ItemMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("ItemMesh"));
+    ItemMesh->SetupAttachment(RootComponent);
+    ItemMesh->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+}
+
+void AItem::BeginPlay()
+{
+    Super::BeginPlay();
+
+    CollisionSphere->OnComponentBeginOverlap.AddDynamic(this, &AItem::OnItemOverlap);
+}
+
+void AItem::OnItemOverlap(
+    UPrimitiveComponent* OverlappedComponent,
+    AActor* OtherActor,
+    UPrimitiveComponent* OtherComp,
+    int32 OtherBodyIndex,
+    bool bFromSweep,
+    const FHitResult& SweepResult
+)
+{
+    APlayerCharacter* Player = Cast<APlayerCharacter>(OtherActor);
+
+    if (!Player)
+    {
+        return;
+    }
+
+    UseItem(Player);
+
+    if (bDestroyOnPickup)
+    {
+        Destroy();
+    }
+}
+
+void AItem::UseItem_Implementation(APlayerCharacter* Player)
+{
+}
+
+void AHealthRecoveryItem::UseItem_Implementation(APlayerCharacter* Player)
+{
+    if (!Player)
+    {
+        return;
+    }
+
+    Player->RestoreHealthToFull();
+}
+
+void AWeaponItem::UseItem_Implementation(APlayerCharacter* Player)
+{
+    if (!Player || !WeaponClass)
+    {
+        return;
+    }
+
+    Player->EquipWeapon(WeaponClass);
+}
---
+++ Source/TemaProject03/Item/Item.h
@@ -0,0 +1,70 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Item.generated.h"
+
+class USphereComponent;
+class UStaticMeshComponent;
+class UPrimitiveComponent;
+class APlayerCharacter;
+class AWeaponBase;
+
+UCLASS()
+class TEMAPROJECT03_API AItem : public AActor
+{
+    GENERATED_BODY()
+
+public:
+    AItem();
+
+protected:
+    virtual void BeginPlay() override;
+
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Item")
+    USphereComponent* CollisionSphere;
+
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Item")
+    UStaticMeshComponent* ItemMesh;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Item")
+    bool bDestroyOnPickup = true;
+
+    UFUNCTION()
+    void OnItemOverlap(
+        UPrimitiveComponent* OverlappedComponent,
+        AActor* OtherActor,
+        UPrimitiveComponent* OtherComp,
+        int32 OtherBodyIndex,
+        bool bFromSweep,
+        const FHitResult& SweepResult
+    );
+
+public:
+    UFUNCTION(BlueprintNativeEvent, BlueprintCallable, Category = "Item")
+    void UseItem(APlayerCharacter* Player);
+
+    virtual void UseItem_Implementation(APlayerCharacter* Player);
+};
+
+UCLASS()
+class TEMAPROJECT03_API AHealthRecoveryItem : public AItem
+{
+    GENERATED_BODY()
+
+public:
+    virtual void UseItem_Implementation(APlayerCharacter* Player) override;
+};
+
+UCLASS()
+class TEMAPROJECT03_API AWeaponItem : public AItem
+{
+    GENERATED_BODY()
+
+public:
+    virtual void UseItem_Implementation(APlayerCharacter* Player) override;
+
+protected:
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Item|Weapon")
+    TSubclassOf<AWeaponBase> WeaponClass;
+};
